fbb3f8122c0990b0f04c1b41dbf32cf759043499
Luka967 <15956695+Luka967@users.noreply.github.com>
2025-05-16 19:03:15 +0200

    0.8.14

--- script/events-persist.lua
+++ script/events-persist.lua
@@ -2,9 +2,6 @@ local state = require("state")
 
 script.on_init(function ()
     state.retrieve_from_storage()
-    for _, surface in pairs(game.surfaces) do
-        state.landing_pads.retrieve_all(surface.index)
-    end
 end)
 
 script.on_load(function ()

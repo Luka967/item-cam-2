3755af9532322b8e02a7d009cad1e938588bae68
Luka967 <15956695+Luka967@users.noreply.github.com>
2025-07-19 21:48:01 +0200

    Add migration

--- migrations/0.10.0_refactor_self_managed_focuses.lua
+++ migrations/0.10.0_refactor_self_managed_focuses.lua
@@ -1,4 +1,4 @@
-storage.focus_new_id = {1}
+storage.focus_new_id = 1
 
 --- @module "state"
 local state = require("__item-cam-2__.script.state")
@@ -7,15 +7,24 @@ state.retrieve_from_storage()
 --- @type table<integer, FocusInstance>
 local remapped_focuses = {}
 for player_idx, focus in pairs(state.focuses) do
-    focus.id = state.focus_new_id[1]
+    focus.id = storage.focus_new_id
+    focus.running = focus.valid and focus.watching ~= nil
     focus.tags = {
         self_managed = true,
         player_idx = player_idx
     }
-    state.focus_new_id[1] = state.focus_new_id[1] + 1
+    storage.focus_new_id = storage.focus_new_id + 1
+
+    for _, controllable in ipairs(focus.controlling) do
+        if controllable.type == "player" then
+            controllable.camera = controllable.camera_element
+            controllable.camera_element = nil
+        end
+    end
 
     remapped_focuses[focus.id] = focus
 end
 
 storage.focuses = remapped_focuses
+state.focus_new_id = storage.focus_new_id
 state.focuses = remapped_focuses
